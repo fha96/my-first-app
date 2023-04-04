3b77126278bcb521bac18f0c34f18ee445f224ac
fha96 <fahadalrimawe96@hotmail.com>
2023-04-04 15:57:26 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-app';
+  name = 'Fahad Zidan';
+
+
+  hit(){
+    console.log(this.name);
+  }
 }
