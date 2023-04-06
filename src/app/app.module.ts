6ea8ee0dc35f8e0c950501d6549c59c695383678
fha96 <fahadalrimawe96@hotmail.com>
2023-04-06 16:54:14 +0300

    feat(app): generate new component and applies dataBinding methodologies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { WarningAlert } from './warningalert/warning.component';
 import { SuccessalertComponent } from './successalert/successalert.component';
+import { UserinfoComponent } from './userinfo/userinfo.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     WarningAlert,
-    SuccessalertComponent
+    SuccessalertComponent,
+    UserinfoComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
